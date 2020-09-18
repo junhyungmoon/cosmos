3491b1bd58b42633a3af0ad933127028c4465cd5
junhyungmoon <jh.moon.cs@gmail.com>
2020-09-18 11:59:21 +0900

    Upload data cropping function

--- functions.py
+++ functions.py
@@ -1,6 +1,26 @@
-def crop_stages(inFile, outFile):
-    ### Store timestamps of CHECKs to buffer
-    timearray = []
+def crop_stages(inFile, outFilePath, timestampList, stageLabelList):
+    outFileList = []
+    for stage in stageLabelList:
+        tmpFile = open(outFilePath + stage + ".csv", "w")
+        outFileList.append(tmpFile)
+
+    stageIdx = -1
+    while True:
+        line = inFile.readline()
+        if not line:
+            break
+        lineData = [x.strip() for x in line.split(',')]
+        for a in range(len(stageLabelList)):
+            if timestampList[a*2] <= float(lineData[0]) <= timestampList[a*2+1]:
+                stageIdx = a
+                break
+            else:
+                stageIdx = -1
+        if stageIdx >= 0:
+            outFileList[stageIdx].write(line)
+
+    for fileIdx in range(len(stageLabelList)):
+        outFileList[fileIdx].close()
 
 def extract_ecg_features(inFile, outFile, winSize, shift):
     ### Store timestamps of CHECKs to buffer
--- main.py
+++ main.py
@@ -1,45 +1,39 @@
 from numpy import *
 from codes.functions import crop_stages, extract_ecg_features, extract_gsr_features, extract_resp_features
 
-
-PcortisolScoreList = [3,4,6,7,9,10,11,12,13,14,16,17,18,20,21,23,24,26,28,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,63,64,65,100] # 40 subjects
-
-crop_stages()
+PcortisolScoreList = [3]#,4,6,7,9,10,11,12,13,14,16,17,18,20,21,23,24,26,28,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,63,64,65,100] # Indices of 40 subjects
+postfixList = ['E_gsr']#, 'Z_ecg', 'Z_bre']
 
 # 1. Crop partial data according to the beginning and the end of each experimental stage
-rootPath = "D:\strEsstimate\exp_data_2nd\\"
-sensorFilePath = rootPath + "D:\strEsstimate\exp_data_2nd\\"
-timeFilePath = rootPath + "D:\strEsstimate\exp_data_2nd\\"
-try:
-    sensorFile = open(sensorFilePath, "r")
-    timeFile = open(timeFilePath, "r")
-except FileNotFoundError as e:
-    print("No file error: " + sensorFilePath)
-else:
-    # read timestamps of "CHECK"s which are either the beginning or the end of each experimental stage
-    timestampArr = []
-    while True:
-        line = timeFile.readline()
-        if not line:
-            break
+rootPath = "E:\mjh\study\cosmos\data\\2nd_exp\\"
+sensorFilePath = rootPath
+timeFilePath = rootPath + "timestamp\\"
+for subNum in PcortisolScoreList:
+    try:
+        timeFileName = "P" + str(subNum) + "_t.txt"
+        timeFile = open(timeFilePath + timeFileName, "r")
+    except FileNotFoundError as e:
+        print("No file error: Subject " + str(subNum))
+    else:
+        # read timestamps of "CHECK"s which are either the beginning or the end of each experimental stage
+        timestampList = []
+        while True:
+            line = timeFile.readline()
+            if not line:
+                break
+            lineData = [x.strip() for x in line.split('\n')]
+            timestampList.append(float(lineData[0]))
+        timeFile.close()
+        numOfStages = int(len(timestampList) / 2)  # number of experimental stages
 
-        lineData = [x.strip() for x in line.split('\n')]
-        timestampArr.append(float(lineData[0]))
-    timeFile.close()
-    numOfStages = int(len(timestampArr) / 2)  # number of experimental stages
+        for postfix in postfixList:
+            sensorFileName = "P" + str(subNum) + "_" + postfix
+            sensorFile = open(sensorFilePath + sensorFileName + ".csv", "r")
 
-    stageLabelArr = ['r1', 'ck', 's1', 'ip', 'ie', 's2', 'vs', 'e1', 'e2', 'r2']
+            stageLabelList = ['r1', 'ck', 's1', 'ip', 'ie', 's2', 'vs', 'e1', 'e2', 'r2']
+            outFilePath = rootPath + "Cropped files\\" + sensorFileName + "_"
+            crop_stages(sensorFile, outFilePath, timestampList, stageLabelList)
 
-    firstIdx = 0
-    lastIdx = numOfStages
-    if int(len(stageLabelArr)) != numOfStages:
-        firstIdx = 1
-        lastIdx = numOfStages
-
-    for stage in stageLabelArr:
-        outFile = open(rootPath + stage, "w")
-        crop_stages(sensorFile, outFile)
-        sensorFile.seek(0,0)
-        outFile.close()
+            sensorFile.close()
 
 
